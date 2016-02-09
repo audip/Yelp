e7b767434ad16cc50e61dea1e02549b2f930e941
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-08 20:41:24 -0600

    Round the image corners

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -33,6 +33,9 @@ class BusinessCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        thumbImageView.layer.cornerRadius = 5
+        thumbImageView.clipsToBounds = true
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
